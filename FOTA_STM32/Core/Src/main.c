5eab43118f5d09c8eaab747b1c9cd10c316a6ad7
Ehab Magdy Abdullah <132620660+EhabMagdyy@users.noreply.github.com>
2024-05-31 03:22:51 +0300

    first commit

--- FOTA_STM32/Core/Src/main.c
+++ FOTA_STM32/Core/Src/main.c
@@ -1,21 +1,9 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+/*****************************************************************************************************************************
+**********************************    Author  : Ehab Magdy Abdullah                      *************************************
+**********************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *************************************
+**********************************    Youtube : https://www.youtube.com/@EhabMagdyy      *************************************
+******************************************************************************************************************************/
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "crc.h"
